07b35bc9c3aeac964b6d032c26b6e486395e41bf
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-09-26 23:47:34 -0300

    Basic bootstrap toolbar

--- src-cljs/friendly/core.cljs
+++ src-cljs/friendly/core.cljs
@@ -1,4 +1,5 @@
-(ns friendly.core)
+(ns friendly.core
+  (:require [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
@@ -7,12 +8,35 @@
 (defn by-id [elem-id]
   (.getElementById js/document elem-id))
 
+;; GLOBAL STATE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def user (atom {:email "denis@clojurecup.com"
+                 :icon "https://secure.gravatar.com/avatar/d9dbd0d79255e58265b5f7597e15eb9a?s=24"}))
+
+(defn toolbar []
+  [:div.container-fluid
+   [:div.navbar-header
+    [:button.navbar-toggle.collapsed
+     {:type "button" :data-toggle "collapse" :data-target ".navbar-collapse"}
+     [:span.sr-only "Toggle navigation"]
+     [:span.icon-bar] [:span.icon-bar] [:span.icon-bar]]
+    [:a.navbar-brand {:href "#"} "Friendly Reader"]]
+   [:div {:class "navbar-collapse collapse"}
+    (when (:email @user)
+      [:ul.nav.navbar-nav.navbar-right
+       [:li.dropdown
+        [:a.dropdown-toggle {:href "" :data-toggle "dropdown"}
+         [:img {:height 24 :width 24 :src (:icon @user) :style {:margin "0px 8px 0px"}}
+          (:email @user)
+          [:span.caret ""]]
+         [:ul.dropdown-menu {:role "menu"}
+          [:li
+           [:a {:href "/logout"}
+            [:span {:class "glyphicon glyphicon-off"}] " Logout"]]]]]])
+    ]
+   ])
+
+
 ;; MAIN APP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn launch-app []
-  (let [app (by-id "app")]
-    (set! (.-innerHTML app) "<h1>Loaded from CLJS!</h1>")))
-
-(launch-app)
-
-
+(reagent/render-component (fn [] [toolbar]) (by-id "navbar"))
