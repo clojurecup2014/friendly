b2af09784fac90bfcd7d94ff6a3af03a6d4fc5bb
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-09-26 21:41:42 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to friendly
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/friendly/core_test.clj
@@ -0,0 +1,7 @@
+(ns friendly.core-test
+  (:require [clojure.test :refer :all]
+            [friendly.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
