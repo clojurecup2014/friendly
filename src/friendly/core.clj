1d571e506528d17d4db113f56e8fa23e071e5bde
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2014-09-27 10:54:06 -0300

    Switch from OpenID to OAuth2, cleanup

--- src/friendly/core.clj
+++ src/friendly/core.clj
@@ -1,65 +1,119 @@
 (ns friendly.core
-  (:require [cemerick.friend :as friend]
-            (cemerick.friend [openid :as openid]
-                             [workflows :as workflows])
-            [ring.middleware.reload :as reload]
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [cemerick.friend :as friend]
+            [friend-oauth2.workflow :as oauth2]
+            [friend-oauth2.util :refer [format-config-uri]]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds])
+            [ring.adapter.jetty :as jetty]
             [ring.util.response :as response])
-  (:use [compojure.core :only [defroutes GET POST DELETE]]
-        [ring.middleware.file-info :only [wrap-file-info]]
+  (:use [ring.middleware.file-info :only [wrap-file-info]]
         [ring.middleware.json :only [wrap-json-response wrap-json-body]]
         [ring.middleware.params :only [wrap-params]]
         [ring.middleware.resource :only [wrap-resource]]
-        [ring.middleware.session :only [wrap-session]]
-        [org.httpkit.server :only [run-server]]))
+        [ring.middleware.session :only [wrap-session]]))
 
 ;; HELPERS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn in-dev? []
   (= "denis" (System/getenv "USER")))
 
+;; OAUTH2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Don't forget to configure the Google consent screen for this app
+;; See also http://stackoverflow.com/a/25762444/483566
+
+(defn credential-fn [token]
+  ;; lookup token in DB or whatever to fetch appropriate :roles
+  ;; eg. GET "https://www.googleapis.com/plus/v1/people/me?access_token=TOKEN" and parse JSON,
+  ;; which contains:
+  ;;    { ...
+  ;;    "emails": [{"value": "denis.fuenzalida@gmail.com","type": "account"}]
+  ;;    ...}
+
+  (println token)
+  {:identity token :roles #{::user}})
+
+(def client-config
+  {;; APP: friendly-reader-777
+   :client-id "981371857470-e6nscou0m1393krkrgttqqmtcr6ne36e.apps.googleusercontent.com"
+   :client-secret "5TTsogg_Z-5kxTusdXy7E208"
+
+   :callback {:domain "http://localhost:3000" :path "/oauth2callback"}})
+
+(def uri-config
+  {:authentication-uri {:url "https://accounts.google.com/o/oauth2/auth"
+                        :query {:client_id (:client-id client-config)
+                                :response_type "code"
+                                :redirect_uri (format-config-uri client-config)
+                                :scope "https://www.googleapis.com/auth/userinfo#email"
+                                }}
+
+   :access-token-uri {:url "https://accounts.google.com/o/oauth2/token"
+                      :query {:client_id (:client-id client-config)
+                              :client_secret (:client-secret client-config)
+                              :grant_type "authorization_code"
+                              :redirect_uri (format-config-uri client-config)}}})
+
 ;; ROUTES ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defroutes routes
-  (GET "/" []
+(defroutes ring-app
+
+  (GET "/" request
+       (println "Session in '/' contains:" (:session request))
        {:status 200
         :body (slurp "resources/public/index.html")})
 
-  (GET "/logout" request
-       (friend/logout* (response/redirect "/")))
+  (GET "/hello" request
+       (friend/authorize #{::user}
+                         (println "*** /hello ***")
+                         (println (:session request))
+                         (response/redirect "/")))
 
-  )
+  (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
 
 ;; MIDDLEWARE ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (def friend-configuration
   {:allow-anon? true
-   :default-landing-uri "/"
-   :workflows [(openid/workflow
-                :openid-uri "/login-openid"
-                :realm (if (in-dev?) "http://localhost:3000" "http://friendly.clojurecup.com/")
-                :credential-fn identity)]})
+   :workflows [(oauth2/workflow
+                {:client-config client-config
+                 :uri-config uri-config
+                 :credential-fn credential-fn})]})
 
 (defn wrap-logging [handler]
   (fn [{:keys [remote-addr request-method uri] :as request}]
     (println remote-addr (.toUpperCase (name request-method)) uri)
     (handler request)))
 
-(def app (-> routes
-             (friend/authenticate friend-configuration)
-             (wrap-resource "public") ;; serve from "resources/public"
-             (wrap-resource "/META-INF/resources") ;; resources from WebJars
-             (wrap-json-body {:keywords? true})
-             wrap-json-response
-             wrap-params
-             wrap-file-info ;; sends the right headers for static files
-             wrap-session ;; required fof openid to save data in the session
-             wrap-logging))
+(def app (-> ring-app
+              (friend/authenticate friend-configuration)
+              (wrap-resource "public") ;; serve from "resources/public"
+              (wrap-resource "/META-INF/resources") ;; resources from WebJars
+              (wrap-json-body {:keywords? true})
+              wrap-json-response
+              wrap-params
+              wrap-file-info ;; sends the right headers for static files
+              wrap-logging
+              wrap-session ;; required fof openid to save data in the session
+              handler/site))
+
+;; Jetty-based
+
+(defn -main [& args]
+  (println "Running Friendly server...")
+  ;; (let [handler (if (in-dev?)
+  ;;                 (reload/wrap-reload app) app)];; only reload when dev
+    (jetty/run-jetty app {:port 3000}))
 
 ;; HTTP-Kit based
 
-(defn -main [& args] ;; entry point, lein run will pick up and start from here
-  (let [handler (if (in-dev?)
-                  (reload/wrap-reload app) ;; only reload when dev
-                  app)]
-    (println "Running server...")
-    (run-server handler {:port 3000})))
+;; (defn -main [& args] ;; entry point, lein run will pick up and start from here
+;;   (let [handler (if (in-dev?)
+;;                   (reload/wrap-reload app) ;; only reload when dev
+;;                   app)]
+;;     (println "Running server...")
+;;     (run-server handler {:port 3000})))
+
